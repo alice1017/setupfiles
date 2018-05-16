a93afdf2b65c92369671ba01a671f7023e063844
Alice1017 <genocidedragon@gmail.com>
2018-05-16 10:55:56 +0900

    ENHANCED: the INSTALL variable apt-get or yum

--- lib/vars.sh
+++ lib/vars.sh
@@ -9,9 +9,21 @@ SRCDIR="$HOME/local/src"
 
 # ALIAS
 SU="sudo"
-INSTALL="apt-get -qq install -y"
 DOWNLOAD="wget -q --no-check-certificate"
 EXTRACT="tar xf"
 
 # SCRIPT
 SPINNER="${BINDIR}/spinner"
+
+# CHECK
+source "$LIBDIR/env.sh"
+
+if has "apt-get"; then
+
+    INSTALL="apt-get -qq install -y"
+
+elif has "yum"; then
+
+   INSTALL="yum -q -y install"
+
+fi
