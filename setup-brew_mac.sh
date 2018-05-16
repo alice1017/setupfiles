9d4a42b62595a286198cd653b59c69489003f3dd
Alice1017 <genocidedragon@gmail.com>
2018-05-16 11:21:56 +0900

    IMPLEMENTED: setup script to install homebrew

---
+++ setup-brew_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load library
+source "$HOME/setupfiles/lib/load.sh"
+
+# Define variables
+RUBY="/usr/bin/ruby"
+URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
+
+CMD="${RUBY} -e $(curl -fsSL ${URL}) > /dev/null 2>&1"
+
+# Install
+$SPINNER "$CMD" "" "Installing homebrew"
