e18a7bb9dc00bdd5520424119183351bbbc78954
Alice1017 <genocidedragon@gmail.com>
2018-05-18 00:38:11 +0900

    FIXED: setup brew script to run correctly

--- setup-brew_mac.sh
+++ setup-brew_mac.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Load library
-source "$HOME/setupscripts/lib/load.sh"
-
-# Define variables
-RUBY="/usr/bin/ruby"
-URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
-
-CMD="${RUBY} -e $(curl -fsSL ${URL}) > /dev/null 2>&1"
-
-# Install
-$SPINNER "$CMD" "" "Installing homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
