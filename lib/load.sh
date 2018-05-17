0ef920034b74cd865e13c2d7aa5b2246e766e1ed
Alice1017 <genocidedragon@gmail.com>
2018-05-17 10:23:15 +0900

    ENHANCED: added 'lib/functions.sh' to loader

--- lib/load.sh
+++ lib/load.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Define variables
-LIBPATH="$HOME/setupfiles/lib/"
+LIBPATH="$HOME/setupscripts/lib/"
 
 # Load scripts
 source "$LIBPATH/env.sh"
 source "$LIBPATH/vars.sh"
+source "$LIBPATH/functions.sh"
 source "$LIBPATH/init.sh"
