995ba99ad4d0e2af27205cbcc8a4ae24fb000a7e
Alice1017 <genocidedragon@gmail.com>
2018-05-18 01:34:43 +0900

    TESTED: new functions

--- tests/test_functions.sh
+++ tests/test_functions.sh
@@ -5,22 +5,52 @@ source "$HOME/setupscripts/lib/load.sh"
 
 testcase_download() {
     download "http://httpbin.org"
-    test -e "$SRCDIR/index.html"
     assert_equal $? 0
+    assert_true $(test -e "${SRCDIR}/index.html")
     rm "$SRCDIR/index.html"
 }
 
-testcase_tar_extract() {
+testcase_search_string() {
+    assert_true $(search_string "abs" "a")
+    assert_true $(search_string "abs" "b")
+    assert_true $(search_string "abs" "s")
+    assert_true $(search_string "abs" "ab")
+    assert_true $(search_string "abs" "bs")
+    assert_true $(search_string "abs" "abs")
+}
+
+testcase_archive_detect() {
+    assert_equal $(archive_detect "a.tar.gz") "tar.gz"
+    assert_equal $(archive_detect "a.zip") "zip"
+}
+
+testcase_extract_tar() {
     local url="https://github.com/ueokande/bashtub/archive/v0.2.tar.gz"
     local file="$(basename "$url")"
     local dir="bashtub-0.2"
     local filepath="${SRCDIR}/${file}"
     local dirpath="${SRCDIR}/${dir}"
 
-    assert_true "download "$url""
-    assert_true "[ -e "$filepath" ]"
-    assert_true "tar_extract "$filepath""
-    assert_true "[ -e "$dirpath" ]"
+    assert_true $(download "$url")
+    assert_true $([ -e "$filepath" ])
+    assert_true $(extract "$filepath")
+    assert_true $([ -e "$dirpath" ])
+
+    rm "$filepath"
+    rm -rf "$dirpath"
+}
+
+testcase_extract_zip() {
+    local url="https://github.com/alice1017/setupscripts/archive/master.zip"
+    local file="$(basename "$url")"
+    local dir="setupscripts-master"
+    local filepath="${SRCDIR}/${file}"
+    local dirpath="${SRCDIR}/${dir}"
+
+    assert_true $(download "$url")
+    assert_true $([ -e "$filepath" ])
+    assert_true $(extract "$filepath")
+    assert_true $([ -e "$dirpath" ])
 
     rm "$filepath"
     rm -rf "$dirpath"
