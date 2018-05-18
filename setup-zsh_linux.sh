fe6d7d44a3b328ed3ab5c122e0bbaa6e0703094b
Alice1017 <genocidedragon@gmail.com>
2018-05-18 09:09:40 +0900

    ENHANCED: handling from configure to make clean

--- setup-zsh_linux.sh
+++ setup-zsh_linux.sh
@@ -9,6 +9,22 @@ FILE="zsh-5.5.1.tar.gz"
 DIR="zsh-5.5.1"
 DEPENDENCIES=("libc6" "libcap2" "libtinfo5" "libncursesw5" "libpcre3")
 
+clean() {
+    rm "$SRCDIR/$FILE"
+    rm -rf "$SRCDIR/zsh-5.5.1"
+}
+
+error_msg() {
+    if [ $? != 0 ];then
+        echo "Error occured!!"
+        cat $logfile
+        clean
+        exit 1
+    else
+        rm $logfile
+    fi
+}
+
 # Install dependics
 for dependence in "${DEPENDENCIES[@]}"
 do
@@ -20,11 +36,18 @@ $SPINNER "download ${URL}" "" "Downloading ${FILE}"
 $SPINNER "extract ${FILE}" "" "Extracting ${FILE}"
 
 # make
-pwd
-echo "cd ${DIR}"
-echo "./configure && make && ${SU} make install && make clean"
+cd ${DIR}
 
+local logfile="/tmp/zsh-configure.log"
+$SPINNER "./configure > "$logfile" 2>&1" "" "Configureing before make"
+error_msg
 
-# clean
-rm "$SRCDIR/$FILE"
-rm -rf "$SRCDIR/zsh-5.5.1"
+local logfile="/tmp/zsh-make-install.log"
+$SPINNER "${SU} make install > /dev/null 2>&1" "" "Installing"
+error_msg
+
+local logfile="/tmp/zsh-make-clean.log"
+$SPINNER "make clean > /dev/null 2>&1" "" "Cleaning"
+error_msg
+
+clean
