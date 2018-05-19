852f577644fcba28308e66b87497478c99840683
Alice1017 <genocidedragon@gmail.com>
2018-05-19 20:55:58 +0900

    IMPLEMENTED: display message after install zsh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -7,3 +7,10 @@ source "$LIBPATH/load.sh"
 # make the script name & execute
 script_name=$(make_script_path "zsh")
 exec_script $script_name
+
+# check exit code
+if has "zsh";then
+    log_pass "Zsh installation was successful."
+else
+    log_fail "Zsh installation was failed."
+fi
