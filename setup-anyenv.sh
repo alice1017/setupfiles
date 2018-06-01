4957111705892f159e6f122b91760878cf418f0f
Alice1017 <genocidedragon@gmail.com>
2018-06-01 09:37:25 +0900

    ENHANCED: checking process to more hader

--- setup-anyenv.sh
+++ setup-anyenv.sh
@@ -13,10 +13,46 @@ source "$LIBPATH/load.sh"
 script_name=$(make_script_path "anyenv")
 exec_script $script_name
 
-# check exit code
+# check exists anyenv
 if [ -e "$HOME/.anyenv" ];then
     log_pass "anyenv installation was successful."
 
 else
     log_fail "anyenv installation was failed."
+fi
+
+# check exists pyenv
+if [ -e "$HOME/.anyenv/envs/pyenv" ];then
+    log_pass "pyenv installation was successful."
+
+    if [ -e "$HOME/.anyenv/envs/pyenv/versions/2.7.5" ];then
+        log_pass "Python 2.7.5 installation was successful."
+    else
+        log_fail "Python 2.7.5 installation was failed."
+    fi
+
+    if [ -e "$HOME/.anyenv/envs/pyenv/versions/3.6.0" ];then
+        log_pass "Python 3.6.0 installation was successful."
+    else
+        log_fail "Python 3.6.0 installation was failed."
+    fi
+
+else
+    log_fail "pyenv installation was failed."
+fi
+
+# check exists rbenv
+if [ -e "$HOME/.anyenv/envs/rbenv" ];then
+    log_pass "rbenv installation was successful."
+
+else
+    log_fail "rbenv installation was failed."
+fi
+
+# check exists ndenv
+if [ -e "$HOME/.anyenv/envs/ndenv" ];then
+    log_pass "rbenv installation was successful."
+
+else
+    log_fail "rbenv installation was failed."
 fi
